f3d75783f3f141ee5c0f9e05e187e5ed0d05eeb2
NeroXerith <astar8820@gmail.com>
2024-11-21 21:44:18 +0800

    Added Insertion, Sew, ReducedString

--- src/javaPractice/Main.java
+++ src/javaPractice/Main.java
@@ -1,10 +1,24 @@
 package javaPractice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		Vehicle car1 = new Vehicle("Honda");
+//		
+//		System.out.println(car1.getBrand());
+		List <Integer> arr = new ArrayList<>(Arrays.asList(2,5,1,4,6,3,7,9,10,8));
+//		Insertion first = new Insertion(arr);
+		
+//		String s1 = "ACE";
+//		String s2 = "BDF";
+//		Sew first = new Sew(s1, s2);
+		
+		String reduced = "aaabccddd";
+		ReducedString first = new ReducedString(reduced);
 	}
 
 }
